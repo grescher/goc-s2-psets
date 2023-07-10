812f0bbecfc530aba574018573e5525186f50fc2
Serhii Chernov <gres.cher@icloud.com>
2023-07-11 02:12:18 +0300

    Add the calculation of the avg age per book

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 |  # | Короткий опис                                                  | Умова | Рішення |
 |---:|:---------------------------------------------------------------|-------|---------|
 |  1 | Вивести усіх користувачів таблицею у консоль через `Printf`... | [#1](https://github.com/grescher/goc-s2-psets/issues/1) | [p1.0](https://github.com/grescher/goc-s2-psets/releases/tag/p1.0) |
-|  2 | До таблиці додати перелік книжок, порахувати середній вік...   | [#2](https://github.com/grescher/goc-s2-psets/issues/2) | |
+|  2 | До таблиці додати перелік книжок, порахувати середній вік...   | [#2](https://github.com/grescher/goc-s2-psets/issues/2) | [p2.0](https://github.com/grescher/goc-s2-psets/releases/tag/p2.0) |
--- main.go
+++ main.go
@@ -1,15 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	users := UserSlice(Users())
 	PrintData(users, UserHeaders)
-	fmt.Println("Books Stats:")
+	fmt.Println("\nBooks Stats:")
 
+	books := avgAgeOfReadersPerBook(users)
+	PrintData(books, APBHeaders)
 }
 
 func PrintData(data TablePrinter, headers []string) {
 	table := data.NewTable(headers)
 	table.Print()
+}
+
+type AgePerBook map[string]int
+
+var APBHeaders = []string{"Name", "Avg. Age"}
+
+func avgAgeOfReadersPerBook(users UserSlice) AgePerBook {
+	tmp := make(map[string]float64)
+	for _, u := range users {
+		for _, book := range u.Books {
+			if _, ok := tmp[book]; !ok {
+				tmp[book] = float64(u.Age)
+			}
+			tmp[book] = (float64(Age(u.Age).Verify()) + tmp[book]) / 2.0
+		}
+	}
+
+	apb := make(map[string]int)
+	for k, v := range tmp {
+		apb[k] = int(math.Round(v))
+	}
+
+	return apb
+}
+
+func (a AgePerBook) NewTable(headers []string) (res Table) {
+	res.Header = headers
+	for name, age := range a {
+		field := make(RowField)
+		field[res.Header[0]] = Name(name).String()
+		field[res.Header[1]] = Age(age).String()
+
+		res.Rows = append(res.Rows, field)
+	}
+	return res
 }
