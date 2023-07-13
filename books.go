c4b413e3dae6636e2d7bfde9e1faed0079e6eda7
Serhii Chernov <gres.cher@icloud.com>
2023-07-13 19:00:15 +0300

    Move Book funcs to a separate file. Implement sorting

---
+++ books.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"math"
+
+	"golang.org/x/exp/slices"
+)
+
+type AvgAgePerBook struct {
+	BookTitle string
+	AvgAge    int
+}
+
+var APBHeaders = []string{"Book Title", "Avg.Age"}
+
+func avgAgeOfReadersPerBook(users UserSlice) (apb AvgAgePerBookSlice) {
+	tmp := make(map[string]float64)
+	for _, u := range users {
+		for _, book := range u.Books {
+			if _, ok := tmp[book]; !ok {
+				tmp[book] = float64(u.Age)
+			}
+			tmp[book] = (float64(Age(u.Age).Verify()) + tmp[book]) / 2.0
+		}
+	}
+
+	for k, v := range tmp {
+		var ele AvgAgePerBook
+		ele.BookTitle = k
+		ele.AvgAge = int(math.Round(v))
+		apb = append(apb, ele)
+	}
+
+	return apb
+}
+
+type AvgAgePerBookSlice []AvgAgePerBook
+
+func (a AvgAgePerBookSlice) SortByAge() {
+	b := []AvgAgePerBook(a)
+	slices.SortFunc[AvgAgePerBook](b, func(x, y AvgAgePerBook) bool {
+		return x.AvgAge < y.AvgAge
+	})
+}
+
+func (a AvgAgePerBookSlice) NewTable(headers []string) (res Table) {
+	res.Header = headers
+	for _, ele := range a {
+		field := make(RowField)
+		field[res.Header[0]] = Name(ele.BookTitle).String()
+		field[res.Header[1]] = Age(ele.AvgAge).String()
+
+		res.Rows = append(res.Rows, field)
+	}
+	return res
+}
--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
 	users := UserSlice(Users())
@@ -11,45 +8,11 @@ func main() {
 	fmt.Println("\nBooks Stats:")
 
 	books := avgAgeOfReadersPerBook(users)
+	books.SortByAge()
 	PrintData(books, APBHeaders)
 }
 
 func PrintData(data TablePrinter, headers []string) {
 	table := data.NewTable(headers)
 	table.Print()
-}
-
-type AgePerBook map[string]int
-
-var APBHeaders = []string{"Name", "Avg. Age"}
-
-func avgAgeOfReadersPerBook(users UserSlice) AgePerBook {
-	tmp := make(map[string]float64)
-	for _, u := range users {
-		for _, book := range u.Books {
-			if _, ok := tmp[book]; !ok {
-				tmp[book] = float64(u.Age)
-			}
-			tmp[book] = (float64(Age(u.Age).Verify()) + tmp[book]) / 2.0
-		}
-	}
-
-	apb := make(map[string]int)
-	for k, v := range tmp {
-		apb[k] = int(math.Round(v))
-	}
-
-	return apb
-}
-
-func (a AgePerBook) NewTable(headers []string) (res Table) {
-	res.Header = headers
-	for name, age := range a {
-		field := make(RowField)
-		field[res.Header[0]] = Name(name).String()
-		field[res.Header[1]] = Age(age).String()
-
-		res.Rows = append(res.Rows, field)
-	}
-	return res
 }
